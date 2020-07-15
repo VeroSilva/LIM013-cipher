39f12ebb20f30ceeadee140c7e067ba937158bab
Veronica Silva <veronicasilvabarreto@gmail.com>
2020-07-15 18:48:19 -0500

    Funciones de cifrado y esqueleto html

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
 const cipher = {
-  // ...
+    // ...
 };
 
 export default cipher;
+
+let offsetUser = document.getElementById("offsetC");
+
+// Cortar el string
+
+let buttonCipher = document.getElementById("cifrar-button");
+buttonCipher.addEventListener("click", cipherAction);
+
+function cipherAction() {
+    let cipherTxtUser = document.getElementById("mensaje-acifrar").value;
+    let cutTxt = cipherTxtUser.split("");
+    let offsetU = document.getElementById("offsetC").value;
+    let txtChar = [];
+
+    cutTxt.forEach(function(caracter) {
+        let operation = ((((caracter.charCodeAt()) - 65 + offsetU) % 26) + (caracter.charCodeAt()));
+        console.log(operation);
+        txtChar.push(String.fromCharCode(operation));
+    });
+
+    console.log(txtChar);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//Boton de validación
+
+const letsValidate = document.getElementById("validationPassButton");
+letsValidate.addEventListener("click", validatePass);
+
+function validatePass() {
+    const correctPassword = "123";
+    let userAnswer = document.getElementById("password").value;
+
+    if (userAnswer == correctPassword) {
+        document.getElementById("screen_1").classList.add("hide");
+        document.getElementById("screen_1").classList.remove("display");
+
+        document.getElementById("screen_2").classList.add("display");
+        document.getElementById("screen_2").classList.remove("hide");
+    } else {
+        alert("Contraseña incorrecta");
+    }
+}
