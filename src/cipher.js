b050b2f80bd112c33cc6391a3146ef9cce329d0c
Veronica Silva <veronicasilvabarreto@gmail.com>
2020-07-21 18:08:21 -0500

    Estilos de formularios, retoques de funcionamiento de los mismos

--- src/cipher.js
+++ src/cipher.js
@@ -8,6 +8,10 @@ export default cipher;
 
 let buttonCipher = document.getElementById("cifrar-button");
 buttonCipher.addEventListener("click", cipherAction);
+buttonCipher.addEventListener("click", function(event) {
+    event.preventDefault()
+});
+
 
 function cipherAction() {
     let cipherTxtUser = document.getElementById("mensaje-acifrar").value;
@@ -23,16 +27,22 @@ function cipherAction() {
         txtChar.push(String.fromCharCode(operation));
     });
 
+    document.getElementById("mensaje-cifrado").classList.add("display");
+    document.getElementById("mensaje-cifrado").classList.remove("hide");
+
     const txtCipherReady = document.getElementById("mensaje-cifrado");
-    let txtNew = document.createElement("p");
-    txtNew.textContent = txtChar.join("");
-    txtCipherReady.appendChild(txtNew);
+    txtCipherReady.innerHTML = "<p>" + txtChar.join("") + "</p>";
+
 }
 
 //DESCIFRADO DEL MENSAJE 
 
 let buttonDcipher = document.getElementById("descifrar-button");
 buttonDcipher.addEventListener("click", dcipherAction);
+buttonDcipher.addEventListener("click", function(event) {
+    event.preventDefault()
+});
+
 
 function dcipherAction() {
     let dcipherTxtUser = document.getElementById("mensaje-descifrar").value;
@@ -45,13 +55,16 @@ function dcipherAction() {
         if ((caracter.charCodeAt()) == 32) {
             operation = 32;
         }
+        if (operation < 65 && operation != 32) {
+            operation += 26;
+        }
         txtChar.push(String.fromCharCode(operation));
     });
 
-    console.log(txtChar);
+    document.getElementById("mensaje-descifrado").classList.add("display");
+    document.getElementById("mensaje-descifrado").classList.remove("hide");
 
     const txtCipherReady = document.getElementById("mensaje-descifrado");
-    let txtNew = document.createElement("p");
-    txtNew.textContent = txtChar.join("");
-    txtCipherReady.appendChild(txtNew);
+    txtCipherReady.innerHTML = "<p>" + txtChar.join("") + "</p>";
+
 }
